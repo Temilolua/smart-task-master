dcc0a1f4a99aff5bef36a8b2ecfd0fa7c216dd20
Temiloluwa Adebayo <temiloluwasamuel05@gmail.com>
2026-08-26 15:24:13 +0100

    Create Task entity

---
+++ src/main/java/com/example/smarttaskmanager/model/Task.java
@@ -0,0 +1,63 @@
+package com.example.smarttaskmanager.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+}
