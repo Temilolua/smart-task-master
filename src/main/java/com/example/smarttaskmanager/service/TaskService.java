73a8b501048b758d525b13fc8026de976ea36825
Temiloluwa Adebayo <temiloluwasamuel05@gmail.com>
2026-08-26 16:29:33 +0100

    Create task service

---
+++ src/main/java/com/example/smarttaskmanager/service/TaskService.java
@@ -0,0 +1,25 @@
+package com.example.smarttaskmanager.service;
+
+import com.example.smarttaskmanager.model.Task;
+import com.example.smarttaskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
